e090c0dcade1a65b9606ce52d6463893623fe68f
Brodie Friday <brodiefriday@Brodies-MacBook-Pro.local>
2022-05-25 13:51:40 -0500

    Added JS code for subtotal button

--- ScoreCard.js
+++ ScoreCard.js
@@ -1,6 +1,7 @@
  let column1Sum = 0;
-    let column2Sum = 0;
-      let column3Sum = 0;
+ let column2Sum = 0;
+ let column3Sum = 0;
+ let subtotalSum = 0;
     function column1(){ //Adds up all of column1
       let G1L = document.getElementById("G1Left").value;
       let G2L = document.getElementById("G2Left").value;
@@ -45,16 +46,17 @@
       let H3D = document.getElementById("H3dif").value;
       let H4D = document.getElementById("H4dif").value;
     column3Sum= (+G1D) + (+G2D) + (+G3D) + (+G4D) + (+G5D) + (+G6D) + (+G7D) + (+H1D) + (+H2D) + (+H3D) + (+H4D);
-      document.getElementById("columnThree").value=column3Sum;
-
-    }
-    function subtotal(){
-        document.getElementById("subtotal").value="hello";
+     document.getElementById("columnThree").value=column3Sum;
     }
     function final(){
       document.getElementById("finalScore").value="hello";
     }
- 
+    
+    function subtotal(){
+      let spreadCredit = document.getElementById("spreadCredit").value;
+      subtotalSum = (+spreadCredit) +(+column1Sum) + (+column2Sum);
+      document.getElementById("subtotal").value= subtotalSum;
+    }
 
 
 
